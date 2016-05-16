a1962c54fe176105321c0aaa39f1e2187d67025e
PaddoInWonderland <martijnrichardk@gmail.com>
2016-05-16 19:44:43 +0200

    Testing

---
+++ test/test.py
@@ -0,0 +1,12 @@
+class Test:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def test(self):
+        await self.bot.say('Test')
+
+
+def setup(bot):
+    n = Test(bot)
+    bot.add_cog(n)
