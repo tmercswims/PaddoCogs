98c959878cbb0fac37519d016f9e66f669a21874
PaddoInWonderland <martijnrichardk@gmail.com>
2016-06-12 17:24:26 +0200

    Info.js

---
+++ info.js
@@ -0,0 +1,7 @@
+{
+    "AUTHOR" : "Paddolicious#8880",
+    "INSTALL_MSG" : "For support join the Discord channel https://discord.gg/0reKmV44060Mtrj1",
+    "NAME" : "PaddoCogs",
+    "SHORT" : "General cogs developped by Paddolicious.",
+    "DESCRIPTION" : ""
+}
