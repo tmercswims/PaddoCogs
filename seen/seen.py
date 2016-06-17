cd616984b92a296f2f56fcc5184419ce7f9e4cd5
PaddoInWonderland <martijnrichardk@gmail.com>
2016-06-17 21:53:02 +0200

    Rewritten

--- seen/seen.py
+++ seen/seen.py
@@ -1,72 +1,84 @@
 from discord.ext import commands
 from .utils.dataIO import fileIO
+import discord
 import os
 
+__version__ = '1.0.0' # gotta start somewhere
 
 class Seen:
-    """Check when someone was last seen."""
+    '''Check when someone was last seen.'''
     def __init__(self, bot):
         self.bot = bot
         self.seen_path = 'data/seen/seen.json'
 
     async def listener(self, message):
-        try:
-            seen = fileIO(self.seen_path, "load")
-            server_id = message.server.id
-            channel_mention = message.channel.mention
-            user_id = message.author.mention
-            user_name = message.author.name
-            last_msg = message.content
+        if not message.channel.is_private and self.bot.user.id != message.author.id:
+            data = fileIO(self.seen_path, 'load')
+            server = message.server
+            author = message.author
+            channel = message.channel
+            content = message.content
             timestamp = message.timestamp
-            if server_id not in seen:
-                seen[server_id] = {}
-            if user_id not in seen[server_id]:
-                seen[server_id][user_id] = {}
-            seen[server_id][user_id]['USERNAME'] = user_name
-            seen[server_id][user_id]['TIMESTAMP'] = str(timestamp)[:-7]
-            seen[server_id][user_id]['MESSAGE'] = last_msg
-            seen[server_id][user_id]['CHANNEL'] = channel_mention
-            fileIO(self.seen_path, "save", seen)
-        except:
-            pass
+            if server.id not in data:
+                data[server.id] = {}
+            if author.id not in data[server.id]:
+                data[server.id][author.id] = {}
+            if author.nick:
+                data[server.id][author.id]['NAME'] = '{} ({})'.format(author.name, author.nick)
+            else:
+                data[server.id][author.id]['NAME'] = author.name
+            data[server.id][author.id]['TIMESTAMP'] = str(timestamp)[:-7]
+            data[server.id][author.id]['MESSAGE'] = content
+            data[server.id][author.id]['CHANNEL'] = channel.mention
+            fileIO(self.seen_path, 'save', data)
+
 
     @commands.command(pass_context=True, no_pm=True, name='seen', aliases=['s'])
-    async def _seen(self, context, username: str):
-        """seen <@username>"""
-        seen = fileIO(self.seen_path, "load")
-        server_id = context.message.server.id
-        username = username.replace('!', '')
-        if server_id in seen:
-            if username in seen[server_id]:
-                timestamp = seen[server_id][username]['TIMESTAMP']
-                last_msg = seen[server_id][username]['MESSAGE']
-                user_name = seen[server_id][username]['USERNAME']
-                channel_name = seen[server_id][username]['CHANNEL']
+    async def _seen(self, context, username: discord.Member):
+        '''seen <@username>'''
+        data = fileIO(self.seen_path, 'load')
+        server = context.message.server
+        author = username
+        if server.id in data:
+            if author.id in data[server.id]:
+                timestamp = data[server.id][author.id]['TIMESTAMP']
+                last_msg = data[server.id][author.id]['MESSAGE']
+                user_name = data[server.id][author.id]['NAME']
+                channel_name = data[server.id][author.id]['CHANNEL']
                 message = '{0} was last seen on `{1} UTC` in {2}, saying: {3}'.format(user_name, timestamp, channel_name, last_msg)
+            elif author.mention in data[server.id]:
+                # legacy
+                timestamp = data[server.id][author.mention]['TIMESTAMP']
+                last_msg = data[server.id][author.mention]['MESSAGE']
+                user_name = data[server.id][author.mention]['NAME']
+                channel_name = data[server.id][author.mention]['CHANNEL']
+                message = '{0} was last seen on `{1} UTC` in {2}, saying: {3}'.format(user_name, timestamp, channel_name, last_msg)
+
             else:
-                message = 'I have not seen {0} yet.'.format(username)
+                message = 'I have not seen {0} yet.'.format(author.name)
         else:
             message = 'I haven\'t seen anyone in this server yet!'
         await self.bot.say('{0}'.format(message))
 
 
 def check_folder():
-    if not os.path.exists("data/seen"):
-        print("Creating data/seen folder...")
-        os.makedirs("data/seen")
+    if not os.path.exists('data/seen'):
+        print('Creating data/seen folder...')
+        os.makedirs('data/seen')
 
 
 def check_file():
     data = {}
-    f = "data/seen/seen.json"
-    if not fileIO(f, "check"):
-        print("Creating default seen.json...")
-        fileIO(f, "save", data)
+    f = 'data/seen/seen.json'
+    if not fileIO(f, 'check'):
+        print('Creating default seen.json...')
+        fileIO(f, 'save', data)
+
 
 
 def setup(bot):
     check_folder()
     check_file()
     n = Seen(bot)
-    bot.add_listener(n.listener, "on_message")
+    bot.add_listener(n.listener, 'on_message')
     bot.add_cog(n)
