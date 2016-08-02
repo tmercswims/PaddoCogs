eb5de1db769cf0841261a028140a73a213390e25
PaddoInWonderland <martijnrichardk@gmail.com>
2016-08-02 19:49:25 +0200

    Diversification

---
+++ caramba/caramba.py
@@ -0,0 +1,13 @@
+class Caramba:
+	def __init__(self, bot):
+		self.bot = bot
+
+	async def listener(self, message):
+		if message.author.id != self.bot.user.id:
+			if message.content.lower().startswith('ayy') or message.content.lower().startswith('aayy'):
+				await self.bot.send_message(message.channel, '¡Caramba!')
+
+def setup(bot):
+    n = Caramba(bot)
+    bot.add_listener(n.listener, "on_message")
+    bot.add_cog(n)
---
+++ redlink/redlink.py
@@ -0,0 +1,17 @@
+class Redlink:
+	def __init__(self, bot):
+		self.bot = bot
+
+	async def listener(self, message):
+		if message.author.id != self.bot.user.id:
+			if '+/r/' in message.content.lower():
+				msg = ''
+				for x in message.content.split(" "):
+					if x.startswith('+/r/'):
+						msg += 'https://www.reddit.com{0}\n'.format(x[1:])
+				await self.bot.send_message(message.channel, msg)
+
+def setup(bot):
+	n = Redlink(bot)
+	bot.add_listener(n.listener, "on_message")
+	bot.add_cog(n)
