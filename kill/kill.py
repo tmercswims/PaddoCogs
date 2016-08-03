60bd66f95c757be307e80119fd08bf388945d7b7
PaddoInWonderland <martijnrichardk@gmail.com>
2016-08-03 10:09:14 +0200

    Update to kills

--- kill/kill.py
+++ kill/kill.py
@@ -12,7 +12,6 @@ class Kill:
         killer = context.message.author.mention
         victim = member.mention
         ways_to_kill = {}
-        ways_to_kill['0'] = '{0} screams in sheer terror and agony as a giant mutated creature with huge muscular arms grab {0}\'s head.'.format(victim)
         ways_to_kill['1'] = '{0} shoves a double barreled shotgun into {1}\'s mouth and squeezes the trigger of the gun, causing {1}\'s head to horrifically explode like a ripe pimple, splattering the young person\'s brain matter, gore, and bone fragments all over the walls and painting it a crimson red.'.format(killer, victim)
         ways_to_kill['3'] = 'Screaming in sheer terror and agony, {0} is horrifically dragged into the darkness by unseen forces, leaving nothing but bloody fingernails and a trail of scratch marks in the ground from which the young person had attempted to halt the dragging process.'.format(victim)
         ways_to_kill['4'] = '{0} takes a machette and starts hacking away on {1}, chopping {1} into dozens of pieces.'.format(killer, victim)
