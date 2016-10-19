0cf2be57b368f7ee897b8a7d62d9d7b7a1d571d6
PaddoInWonderland <martijnrichardk@gmail.com>
2016-10-19 17:15:46 +0200

    Performance updates

--- games/games.py
+++ games/games.py
@@ -4,73 +4,78 @@ from .utils.dataIO import fileIO
 from difflib import SequenceMatcher
 
 class Games:
-    def __init__(self, bot):
-        self.bot = bot
-        self.data_file = 'data/games/games.json'
+	def __init__(self, bot):
+		self.bot = bot
+		self.data_file = 'data/games/games.json'
+		self.data = fileIO(self.data_file, 'load')
 
-    def match(self, a, b):
-        return SequenceMatcher(None, a, b).ratio()
+	def match(self, a, b):
+		return SequenceMatcher(None, a, b).ratio()
 
-    async def listener(self, before, after):
-        before_game = str(before.game)
-        try:
-            after_game = str(after.game)
-        except TypeError:
-            after_game = 'None'
-        server = after.server
+	async def listener(self, before, after):
+		before_game = str(before.game)
+		try:
+			after_game = str(after.game)
+		except TypeError:
+			after_game = 'None'
+		server = after.server
 
-        if not after.bot:
-            if after_game != 'None' and after_game != '':
-                if before_game != after_game:
-                    data = fileIO(self.data_file, 'load')
-                    if server.id not in data:
-                        data[server.id] = {}
-                        data[server.id]['GAMES'] = {}
-                    game_match = ''
-                    for game in data[server.id]['GAMES']:
-                        if self.match(str(game).upper(), after_game.upper()) > 0.89 and self.match(str(game).upper(), after_game.upper()) < 1.0:
-                            game_match = game
-                    if game_match in data[server.id]['GAMES']:
-                        data[server.id]['GAMES'][game_match]['PLAYED'] += 1
-                    elif after_game not in data[server.id]['GAMES']:
-                        data[server.id]['GAMES'][after_game] = {}
-                        data[server.id]['GAMES'][after_game]['PLAYED'] = 1
-                        data[server.id]['GAMES'][after_game]['GAME'] = after_game
-                    else:
-                        data[server.id]['GAMES'][after_game]['PLAYED'] += 1
-                    fileIO(self.data_file, 'save', data)
+		if not after.bot:
+			if after_game != 'None' and after_game != '':
+				if before_game != after_game:
+					if server.id not in self.data:
+						self.data[server.id] = {}
+						self.data[server.id]['GAMES'] = {}
+					game_match = ''
+					for game in self.data[server.id]['GAMES']:
+						if self.match(str(game).upper(), after_game.upper()) > 0.89 and self.match(str(game).upper(), after_game.upper()) < 1.0:
+							game_match = game
+					if game_match in self.data[server.id]['GAMES']:
+						self.data[server.id]['GAMES'][game_match]['PLAYED'] += 1
+					elif after_game not in data[server.id]['GAMES']:
+						self.data[server.id]['GAMES'][after_game] = {}
+						self.data[server.id]['GAMES'][after_game]['PLAYED'] = 1
+						self.data[server.id]['GAMES'][after_game]['GAME'] = after_game
+					else:
+						self.data[server.id]['GAMES'][after_game]['PLAYED'] += 1
 
-    @commands.command(pass_context=True, no_pm=True, name='games')
-    async def _games(self, context):
-        """Shows top 10 most popular games on this server."""
-        server = context.message.server
-        data = fileIO(self.data_file, 'load')
-        if server.id in data:
-            data = data[server.id]['GAMES']
-            games_played = sorted(data, key=lambda x: (data[x]['PLAYED']), reverse=True)
-            message = '```Most popular games played on {}\n\n'.format(server.name)
-            for i, game in enumerate(games_played, 1):
-                if i > 10:
-                    break
-                message+='{:<5}{:<10}\n'.format(i, game)
-            message+='```'
-            await self.bot.say(message)
+	@commands.command(pass_context=True, no_pm=True, name='games')
+	async def _games(self, context):
+		"""Shows top 10 most popular games on this server."""
+		server = context.message.server
+		if server.id in data:
+			data = self.data[server.id]['GAMES']
+			games_played = sorted(data, key=lambda x: (data[x]['PLAYED']), reverse=True)
+			message = '```Most popular games played on {}\n\n'.format(server.name)
+			for i, game in enumerate(games_played, 1):
+				if i > 10:
+					break
+				message+='{:<5}{:<10}\n'.format(i, game)
+			message+='```'
+			await self.bot.say(message)
+
+	async def _store_data(self):
+		await self.bot.wait_until_ready()
+		while self is self.bot.get_cog('Games'):
+			fileIO(self.data_file, 'save', self.data)
+			await asyncio.sleep(60)
 
 def check_folder():
-    if not os.path.exists('data/games'):
-        print('Creating data/games folder...')
-        os.makedirs('data/games')
+	if not os.path.exists('data/games'):
+		print('Creating data/games folder...')
+		os.makedirs('data/games')
 
 def check_file():
-    data = {}
-    f = 'data/games/games.json'
-    if not fileIO(f, 'check'):
-        print('Creating default games.json...')
-        fileIO(f, 'save', data)
+	data = {}
+	f = 'data/games/games.json'
+	if not fileIO(f, 'check'):
+		print('Creating default games.json...')
+		fileIO(f, 'save', data)
 
 def setup(bot):
-    check_folder()
-    check_file()
-    n = Games(bot)
-    bot.add_listener(n.listener, 'on_member_update')
-    bot.add_cog(n)
+	check_folder()
+	check_file()
+	n = Games(bot)
+	bot.add_listener(n.listener, 'on_member_update')
+	bot.loop.create_task(n._store_data())
+	bot.add_cog(n)
--- statistics/statistics.py
+++ statistics/statistics.py
@@ -8,146 +8,152 @@ import time
 import os
 
 class Statistics:
-    """
-    Statistics
-    """
-    def __init__(self, bot):
-        self.bot = bot
-        self.settings = fileIO('data/statistics/settings.json', 'load')
-        self.sent_messages = self.settings['SENT_MESSAGES']
-        self.received_messages = self.settings['RECEIVED_MESSAGES']
-        self.refresh_rate = self.settings['REFRESH_RATE']
+	"""
+	Statistics
+	"""
+	def __init__(self, bot):
+		self.bot = bot
+		self.settings = fileIO('data/statistics/settings.json', 'load')
+		self.sent_messages = self.settings['SENT_MESSAGES']
+		self.received_messages = self.settings['RECEIVED_MESSAGES']
+		self.refresh_rate = self.settings['REFRESH_RATE']
 
-    async def _int(self, n):
-        try:
-            int(n)
-            return True
-        except ValueError:
-            return False
+	async def _int(self, n):
+		try:
+			int(n)
+			return True
+		except ValueError:
+			return False
 
-    @commands.command()
-    async def stats(self):
-        """
-        Retreive statistics
-        """
-        message = await self.retrieve_statistics()
-        await self.bot.say(message)
+	@commands.command()
+	async def stats(self):
+		"""
+		Retreive statistics
+		"""
+		message = await self.retrieve_statistics()
+		await self.bot.say(message)
 
-    @commands.command()
-    async def statsrefresh(self, seconds: int):
-        """
-        Set the refresh rate by which the statistics are updated
+	@commands.command()
+	async def statsrefresh(self, seconds: int):
+		"""
+		Set the refresh rate by which the statistics are updated
 
-        Example: [p]statsrefresh 42
+		Example: [p]statsrefresh 42
 
-        Default: 5
-        """
-        if await self._int(seconds):
-            if seconds < 5:
-                message = '`I can\'t do that, the refresh rate has to be above 5 seconds`'
-            else:
-                self.refresh_rate = seconds
-                self.settings['REFRESH_RATE'] = self.refresh_rate
-                fileIO('data/statistics/settings.json', 'save', self.settings)
-                message = '`Changed refresh rate to {} seconds`'.format(self.refresh_rate)
-        await self.bot.say(message)
+		Default: 5
+		"""
+		if await self._int(seconds):
+			if seconds < 5:
+				message = '`I can\'t do that, the refresh rate has to be above 5 seconds`'
+			else:
+				self.refresh_rate = seconds
+				self.settings['REFRESH_RATE'] = self.refresh_rate
+				fileIO('data/statistics/settings.json', 'save', self.settings)
+				message = '`Changed refresh rate to {} seconds`'.format(self.refresh_rate)
+		await self.bot.say(message)
 
-    @commands.command(no_pm=True)
-    @checks.serverowner_or_permissions(manage_server=True)
-    async def statschannel(self, *channel: discord.Channel):
-        """
-        Set the channel to which the bot will sent its continues updates.
-        Example: [p]statschannel #statistics
-        """
-        if len(channel) > 0:
-            self.settings['CHANNEL_ID'] = str(channel[0].id)
-            fileIO('data/statistics/settings.json', 'save', self.settings)
-            message = 'Channel set to {}'.format(channel[0].mention)
-        elif not self.settings['CHANNEL_ID']:
-            message = 'No channel set!'
-        else:
-            channel = discord.utils.get(self.bot.get_all_channels(), id=self.settings['CHANNEL_ID'])
-            message = 'Current channel is {}'.format(channel.mention)
-        await self.bot.say(message)
+	@commands.command(no_pm=True)
+	@checks.serverowner_or_permissions(manage_server=True)
+	async def statschannel(self, *channel: discord.Channel):
+		"""
+		Set the channel to which the bot will sent its continues updates.
+		Example: [p]statschannel #statistics
+		"""
+		if len(channel) > 0:
+			self.settings['CHANNEL_ID'] = str(channel[0].id)
+			fileIO('data/statistics/settings.json', 'save', self.settings)
+			message = 'Channel set to {}'.format(channel[0].mention)
+		elif not self.settings['CHANNEL_ID']:
+			message = 'No channel set!'
+		else:
+			channel = discord.utils.get(self.bot.get_all_channels(), id=self.settings['CHANNEL_ID'])
+			message = 'Current channel is {}'.format(channel.mention)
+		await self.bot.say(message)
 
-    async def retrieve_statistics(self):
-        name = self.bot.user.name
-        uptime = abs(self.bot.uptime - int(time.perf_counter()))
-        up = datetime.timedelta(seconds=uptime)
-        days = up.days
-        hours = int(up.seconds/3600)
-        minutes = int(up.seconds%3600/60)
-        users = str(len(set(self.bot.get_all_members())))
-        servers = str(len(self.bot.servers))
-        text_channels = 0
-        voice_channels = 0
-        for channel in self.bot.get_all_channels():
-            if channel.type == discord.ChannelType.text:
-                text_channels += 1
-            elif channel.type == discord.ChannelType.voice:
-                voice_channels += 1
-        channels = text_channels + voice_channels
-        message = '**{}** has been up for **{} days, {} hours, and {} minutes**'.format(name, str(days), str(hours), str(minutes))
-        message+= '\n\n'
-        message+= 'Connected to **{}** servers'.format(servers)
-        message+= '\n'
-        message+= 'Seen **{}** users'.format(users)
-        message+= '\n'
-        message+= 'In **{}** channels (**{}** text, **{}** voice)'.format(str(channels), str(text_channels), str(voice_channels))
-        message+= '\n'
-        message+= '**{}** messages received and **{}** messages sent'.format(str(self.received_messages), str(self.sent_messages))
-        message+= '\n'
-        message+= '**{}** active cogs with **{}** commands'.format(str(len(self.bot.cogs)), str(len(self.bot.commands)))
-        message+= '\n'
-        message+= 'API version **{}**'.format(discord.__version__)
-        return message
+	async def retrieve_statistics(self):
+		name = self.bot.user.name
+		uptime = abs(self.bot.uptime - int(time.perf_counter()))
+		up = datetime.timedelta(seconds=uptime)
+		days = up.days
+		hours = int(up.seconds/3600)
+		minutes = int(up.seconds%3600/60)
+		users = str(len(set(self.bot.get_all_members())))
+		servers = str(len(self.bot.servers))
+		text_channels = 0
+		voice_channels = 0
+		for channel in self.bot.get_all_channels():
+			if channel.type == discord.ChannelType.text:
+				text_channels += 1
+			elif channel.type == discord.ChannelType.voice:
+				voice_channels += 1
+		channels = text_channels + voice_channels
+		message = '**{}** has been up for **{} days, {} hours, and {} minutes**'.format(name, str(days), str(hours), str(minutes))
+		message+= '\n\n'
+		message+= 'Connected to **{}** servers'.format(servers)
+		message+= '\n'
+		message+= 'Seen **{}** users'.format(users)
+		message+= '\n'
+		message+= 'In **{}** channels (**{}** text, **{}** voice)'.format(str(channels), str(text_channels), str(voice_channels))
+		message+= '\n'
+		message+= '**{}** messages received and **{}** messages sent'.format(str(self.received_messages), str(self.sent_messages))
+		message+= '\n'
+		message+= '**{}** active cogs with **{}** commands'.format(str(len(self.bot.cogs)), str(len(self.bot.commands)))
+		message+= '\n'
+		message+= 'API version **{}**'.format(discord.__version__)
+		return message
 
-    async def incoming_messages(self, message):
-        if message.author.id == self.bot.user.id:
-            self.sent_messages += 1
-        else:
-            self.received_messages += 1
-        self.settings['SENT_MESSAGES'] = self.sent_messages
-        self.settings['RECEIVED_MESSAGES'] = self.received_messages
-        fileIO('data/statistics/settings.json', 'save', self.settings)
+	async def incoming_messages(self, message):
+		if message.author.id == self.bot.user.id:
+			self.sent_messages += 1
+		else:
+			self.received_messages += 1
+		self.settings['SENT_MESSAGES'] = self.sent_messages
+		self.settings['RECEIVED_MESSAGES'] = self.received_messages
 
-    async def reload_stats(self):
-        await asyncio.sleep(30)
-        while self == self.bot.get_cog('Statistics'):
-            if self.settings['CHANNEL_ID']:
-                msg = await self.retrieve_statistics()
-                channel = discord.utils.get(self.bot.get_all_channels(), id=self.settings['CHANNEL_ID'])
-                messages = False
-                async for message in self.bot.logs_from(channel, limit=1):
-                    messages = True
-                    if message.author.name == self.bot.user.name:
-                        await self.bot.edit_message(message, msg)
-                if not messages:
-                    await self.bot.send_message(channel, msg)
-            else:
-                pass
-            await asyncio.sleep(self.refresh_rate)
+	async def reload_stats(self):
+		await asyncio.sleep(30)
+		while self == self.bot.get_cog('Statistics'):
+			if self.settings['CHANNEL_ID']:
+				msg = await self.retrieve_statistics()
+				channel = discord.utils.get(self.bot.get_all_channels(), id=self.settings['CHANNEL_ID'])
+				messages = False
+				async for message in self.bot.logs_from(channel, limit=1):
+					messages = True
+					if message.author.name == self.bot.user.name:
+						await self.bot.edit_message(message, msg)
+				if not messages:
+					await self.bot.send_message(channel, msg)
+			else:
+				pass
+			await asyncio.sleep(self.refresh_rate)
+
+	async def _store_data(self):
+		await self.bot.wait_until_ready()
+		while self is self.bot.get_cog('Statistics'):
+			fileIO('data/statistics/settings.json', 'save', self.settings)
+			await asyncio.sleep(60)
 
 def check_folder():
-    if not os.path.exists("data/statistics"):
-        print("Creating data/statistics folder...")
-        os.makedirs("data/statistics")
+	if not os.path.exists("data/statistics"):
+		print("Creating data/statistics folder...")
+		os.makedirs("data/statistics")
 
 def check_file():
-    data = {}
-    data['CHANNEL_ID'] = ''
-    data['SENT_MESSAGES'] = 0
-    data['RECEIVED_MESSAGES'] = 0
-    data['REFRESH_RATE'] = 5
-    f = "data/statistics/settings.json"
-    if not fileIO(f, "check"):
-        print("Creating default settings.json...")
-        fileIO(f, "save", data)
+	data = {}
+	data['CHANNEL_ID'] = ''
+	data['SENT_MESSAGES'] = 0
+	data['RECEIVED_MESSAGES'] = 0
+	data['REFRESH_RATE'] = 5
+	f = "data/statistics/settings.json"
+	if not fileIO(f, "check"):
+		print("Creating default settings.json...")
+		fileIO(f, "save", data)
 
 def setup(bot):
-    check_folder()
-    check_file()
-    n = Statistics(bot)
-    bot.add_cog(n)
-    bot.add_listener(n.incoming_messages, "on_message")
-    bot.loop.create_task(n.reload_stats())
+	check_folder()
+	check_file()
+	n = Statistics(bot)
+	bot.add_cog(n)
+	bot.add_listener(n.incoming_messages, "on_message")
+	bot.loop.create_task(n._store_data())
+	bot.loop.create_task(n.reload_stats())
