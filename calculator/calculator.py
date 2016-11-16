5b204b06175d6930620841cde8ecf41c4d2cec68
PaddoInWonderland <martijnrichardk@gmail.com>
2016-11-16 08:51:18 +0100

    Da

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -15,7 +15,7 @@ class Calculator:
         math_filter = re.findall(r'[\[\]\-()*+/0-9=., ]|random|randint|choice|randrange|True|False|if|and|or|else|is|acos|acosh|asin|asinh|atan|atan2|atanh|ceil|copysign|cos|cosh|degrees|e|erf|erfc|exp|expm1|fabs|factorial|floor|fmod|frexp|fsum|gamma|gcd|hypot|inf|isclose|isfinite|isinf|isnan|ldexp|lgamma|log|log10|log1p|log2|modf|nan|pi|pow|radians|sin|sinh|sqrt|tan|tanh', m)
         calculate_stuff = eval(''.join(math_filter))
         if len(str(calculate_stuff)) > 0:
-            em = discord.Embed(color=discord.Color.blue(), description='**Input**\n`{}`\n**Result**\n`{}`'.format(m, calculate_stuff))
+            em = discord.Embed(color=discord.Color.blue(), description='**Input**\n`{}`\n\n**Result**\n`{}`'.format(m, calculate_stuff))
             # em.add_field(inline=False, name='Input', value=m)
             # em.add_field(inline=False, name='\a', value='\a')
             # em.add_field(inline=False, name='Result', value=calculate_stuff)
