f1ba90f1e172e17453829e2f36b7d1f5b6143fa1
PaddoInWonderland <martijnrichardk@gmail.com>
2016-11-16 08:47:19 +0100

    Embedified. Update your damn discord.py

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,7 +1,8 @@
-from discord.ext import commands
 import re
+import discord
 from math import *
 from random import *
+from discord.ext import commands
 
 
 class Calculator:
@@ -14,7 +15,12 @@ class Calculator:
         math_filter = re.findall(r'[\[\]\-()*+/0-9=., ]|random|randint|choice|randrange|True|False|if|and|or|else|is|acos|acosh|asin|asinh|atan|atan2|atanh|ceil|copysign|cos|cosh|degrees|e|erf|erfc|exp|expm1|fabs|factorial|floor|fmod|frexp|fsum|gamma|gcd|hypot|inf|isclose|isfinite|isinf|isnan|ldexp|lgamma|log|log10|log1p|log2|modf|nan|pi|pow|radians|sin|sinh|sqrt|tan|tanh', m)
         calculate_stuff = eval(''.join(math_filter))
         if len(str(calculate_stuff)) > 0:
-            await self.bot.say(calculate_stuff)
+            em = discord.Embed(color=discord.Color.blue())
+            em.add_field(inline=False, name='Input', value=m)
+            em.add_field(inline=False, name='\a', value='\a')
+            em.add_field(inline=False, name='Result', value=calculate_stuff)
+            await self.bot.say(embed=em)
+            # await self.bot.say(calculate_stuff)
 
 
 def setup(bot):
